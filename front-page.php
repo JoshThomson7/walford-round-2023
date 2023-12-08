d009d634d45d30b1ca62508d626dbb173120ca35
JoshThomson7 <josh@fl1.digital>
2023-12-08 11:38:57 +0000

    Ticker edit

--- front-page.php
+++ front-page.php
@@ -8,8 +8,8 @@ get_header();
 AVB::avb_banners(); ?>
 
 <div class="ticker-container">
-    <div class="ticker">
-        <p>ANNOUNCEMENT: OUR NEW WEBSITE HAS LAUNCHED! THIS IS A GREAT PLACE TO SHOW KEY INFO!</p>
+    <div class="ticker-static">
+        <p style="text-align: center;">ANNOUNCEMENT: OUR NEW WEBSITE HAS LAUNCHED! THIS IS A GREAT PLACE TO SHOW KEY INFO!</p>
     </div>
 </div>
 
