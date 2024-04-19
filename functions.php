24cef9f76222cbe30fa56b2d172db8f34ee624ef
JoshThomson7 <josh@fl1.digital>
2024-04-19 11:07:02 +0100

    Remove team

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 require_once 'core/class-fl1.php';
 require_once 'modules/flexible-content/class-fc.php';
 require_once 'modules/advanced-video-banners/functions/class-avb.php';
-require_once 'modules/advanced-team-members/functions/ateam-functions.php';
+// require_once 'modules/advanced-team-members/functions/ateam-functions.php';
 require_once 'modules/fl1-client/class-fl1c.php';
 require_once 'modules/blog/class-fl1-blog-module.php';
 
